281b6ce6468d09702fcf35f59e47057b8f5ded10
Hrishisk99 <hrishikeshs2047@gmail.com>
2026-07-17 16:17:28 +0530

    feat: establish backend foundation

---
+++ server/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Health Check Route
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Military Asset Management API is running",
+  });
+});
+
+export default app;
